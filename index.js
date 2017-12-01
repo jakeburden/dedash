65531a5d4edaa2387c0c7e7e824672a4fbc8b51b
Jake Burden <jake@doge.haus>
2017-12-01 10:24:25 -0500

    add support for remocing hyphen-minus, low line, and hyphen

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = function dedash (str) {
-  return str.replace( /[\u2012\u2013\u2014\u2015\u2053]/g, '' )
+  return str.replace( /[\u2012\u2013\u2014\u2015\u2053\u002D\u005F\u2010]/g, '' )
 }
